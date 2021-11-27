ada14ab255238b2051b6b040185326341fa6117b
ramshaatta17@gmail.com <ramshaatta17@gmail.com>
2021-11-27 21:33:49 +0500

    First commit

---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConsoleApp1
+{
+    [TestClass]
+    public class BasicMaths
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            2.  public class UnitTest1
+        {  
+    [TestMethod]  
+            public void Test_AddMethod()
+            {
+                5.BasicMaths bm = new BasicMaths();
+                6.              double res = bm.Add(10, 10);
+                7.Assert.AreEqual(res, 20);
+                8.          }  
+        [TestMethod]  
+	    public void Test_SubstractMethod()
+            {
+                BasicMaths bm = new BasicMaths();
+                             double res = bm.Substract(10, 10);
+                Assert.AreEqual(res, 0);
+                         }  
+	        [TestMethod]  
+	    public void Test_DivideMethod()
+            {
+                BasicMaths bm = new BasicMaths();
+                             double res = bm.divide(10, 5);
+                Assert.AreEqual(res, 2);
+                         }  
+	        [TestMethod]  
+	    public void Test_MultiplyMethod()
+            {
+                BasicMaths bm = new BasicMaths();
+                         double res = bm.Multiply(10, 10);
+                Assert.AreEqual(res, 100);
+                     }
+
+        }
+    }
+}
---
+++ test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    void print(int i)
+    {
+        Console.WriteLine("Printing int: {0}", i);
+    }
+    void print(double f,string a)
+    {
+        Console.WriteLine("Printing float: {0}", f + a);
+    }
+    public static void Main()
+    {
+        Program p = new Program();
+        p.print(5);
+        p.print(500.654,"Ramsha");
+    }
+}
